23bea067f31ab9db2fb70f336e09d2e453bbd2a6
rival2905 <valdothea@gmail.com>
2024-04-15 20:38:27 +0700

    last update

--- lib/main.dart
+++ lib/main.dart
@@ -1,124 +1,143 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_translate/flutter_translate.dart';
 import 'package:smas_app/Views/SplaceScreen/spalace_screen.dart';
-
 import 'package:smas_app/Helper/Theme.dart';
 import 'package:smas_app/Views/components/Loading/post_card_hr.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var delegate = await LocalizationDelegate.create(
+      fallbackLocale: 'en', supportedLocales: ['en', 'id']);
+
+  
+  runApp(LocalizedApp(delegate, MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var localizationDelegate = LocalizedApp.of(context).delegate;
+
     return GetMaterialApp(
       title: 'SMAS APP',
       theme: lightTheme,
       themeMode: ThemeMode.dark,
       darkTheme: darkTheme,
-      home: const SplaceScreenPageState(),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        localizationDelegate
+      ],
+      supportedLocales: const [
+        Locale('en', ''),
+        Locale('id', ''),
+      ],
+      locale: localizationDelegate.currentLocale,
+      home: SplaceScreenPageState(),
     );
   }
 }
 
-// return GetMaterialApp(
-//       title: 'SMAS APP',
-//       theme: ThemeData(
-
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-        
-//       ),
-//       home: SplaceScreenPageState(),
-//     );
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key? key, this.title}) : super(key: key);
+  final String? title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  void _decrementCounter() => setState(() => _counter--);
+
+  void _incrementCounter() => setState(() => _counter++);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var localizationDelegate = LocalizedApp.of(context).delegate;
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(translate('app_bar.title')),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text(translate('language.selected_message', args: {
+              'language': translate(
+                  'language.name.${localizationDelegate.currentLocale.languageCode}')
+            })),
+            Padding(
+                padding: EdgeInsets.only(top: 25, bottom: 160),
+                child: CupertinoButton.filled(
+                  child: Text(translate('button.change_language')),
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 36.0),
+                  onPressed: () => _onActionSheetPress(context),
+                )),
+            Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Text(translatePlural('plural.demo',_counter))),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.remove_circle),
+                  iconSize: 48,
+                  onPressed: _counter > 0
+                     ? () => setState(() => _decrementCounter())
+                      : null,
+                ),
+                IconButton(
+                  icon: Icon(Icons.add_circle),
+                  color: Colors.blue,
+                  iconSize: 48,
+                  onPressed: () => setState(() => _incrementCounter()),
+                ),
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void showDemoActionSheet(
+      {required BuildContext context, required Widget child}) {
+    showCupertinoModalPopup<String>(
+        context: context,
+        builder: (BuildContext context) => child).then((String? value) {
+      if (value!= null) changeLocale(context, value);
+    });
+  }
+
+  void _onActionSheetPress(BuildContext context) {
+    showDemoActionSheet(
+      context: context,
+      child: CupertinoActionSheet(
+        title: Text(translate('language.selection.title')),
+        message: Text(translate('language.selection.message')),
+        actions: <Widget>[
+          CupertinoActionSheetAction(
+            child: Text(translate('language.name.en')),
+            onPressed: () => Navigator.pop(context, 'en'),
+          ),
+          CupertinoActionSheetAction(
+            child: Text(translate('language.name.id')),
+            onPressed: () => Navigator.pop(context, 'id'),
+          ),
+        ],
+        cancelButton: CupertinoActionSheetAction(
+          child: Text(translate('button.cancel')),
+          isDefaultAction: true,
+          onPressed: () => Navigator.pop(context, null),
+        ),
+      ),
     );
   }
 }
